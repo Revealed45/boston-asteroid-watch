0de6d2f553a068d720309173de9143ae7b56fa17
Revealed45 <Eric@paperbridge.ai>
2026-02-24 11:38:33 -0600

    Add files via upload

--- manage.py
+++ manage.py
@@ -7,18 +7,8 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed?"
-        ) from exc
+        raise ImportError("Couldn't import Django.") from exc
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
-
-# Railway Nixpacks needs this to detect Django
-application = None
-try:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asteroid_tracker.settings')
-    from asteroid_tracker.wsgi import application
-except Exception:
-    pass
